5f96b708464f952d8604ac7f8c08796c3907c80c
Matin ShayanFar <matinshayanfar@pm.me>
2026-01-15 10:49:41 +0300

    Updated gateways

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,9 +5,8 @@ declare enum GATEWAY {
     ZAIN = "ZAIN",
     ASIA_PAY = "ASIA_PAY",
     FAST_PAY = "FAST_PAY",
-    SUPER_QI = "SUPER_QI",
     NASS_WALLET = "NASS_WALLET",
-    YANA = "YANA"
+    CREDIT_CARD = "CREDIT_CARD"
 }
 declare enum PAYMENT_STATUS {
     TIMED_OUT = "TIMED_OUT",
